7ff940f64d0df12839d882165bc541f44c59d009
Mikhail Vyuzhanin <movyuzhanin@avito.ru>
2023-08-24 01:27:51 +0300

    ex-4: ws2 - bind actors data view holder

--- app/src/main/java/com/android/fundamentals/MainActivity.kt
+++ app/src/main/java/com/android/fundamentals/MainActivity.kt
@@ -3,6 +3,7 @@ package com.android.fundamentals
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.fundamentals.workshop01.WS01ActorsFragment
+import com.android.fundamentals.workshop02.WS02ActorsFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                     // TODO: Change fragment according to the workshop #
-                    .replace(R.id.container, WS01ActorsFragment.newInstance())
+                    .replace(R.id.container, WS02ActorsFragment.newInstance())
                     .commit()
         }
     }
--- app/src/main/java/com/android/fundamentals/workshop02/WS02ActorsAdapter.kt
+++ app/src/main/java/com/android/fundamentals/workshop02/WS02ActorsAdapter.kt
@@ -11,6 +11,7 @@ import com.android.fundamentals.R
 import com.android.fundamentals.data.models.Actor
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import java.lang.IllegalArgumentException
 
 // TODO 00: Find TODOs 01-04 in the bottom.
 class WS02ActorsAdapter : RecyclerView.Adapter<ActorsViewHolder>() {
@@ -27,14 +28,21 @@ class WS02ActorsAdapter : RecyclerView.Adapter<ActorsViewHolder>() {
         // TODO 05: Change the default return result, and return constant from the previous step.
         //  If actors are empty, return the constant for the EmptyViewHolder.
         //  Otherwise return the constant for the DataViewHolder.
-        return super.getItemViewType(position)
+        if (actors.isEmpty()) {
+            return _EMPTY
+        }
+        return _ACTORS
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActorsViewHolder {
         // TODO 06: You defined two different view types.
         //  Change the existed return result.
         //  Return the EmptyViewHolder or DataViewHolder depending on the viewType.
-        return EmptyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_actors_empty, parent, false))
+        return when (viewType) {
+            _ACTORS -> DataViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_actors_data, parent, false))
+            _EMPTY -> EmptyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_actors_empty, parent, false))
+            else -> throw IllegalArgumentException()
+        }
     }
 
     override fun onBindViewHolder(holder: ActorsViewHolder, position: Int) {
@@ -43,7 +51,10 @@ class WS02ActorsAdapter : RecyclerView.Adapter<ActorsViewHolder>() {
         //  When holder is the Data Holder, call public method of this holder and provide parameters.
         //  In our workshop, the position of the item inside the recycler is the same
         //  as the position inside our Actors collection.
-        Toast.makeText(holder.itemView.context, "Nothing to bind in default holder", Toast.LENGTH_LONG).show()
+        when (holder) {
+            is DataViewHolder -> holder.actorBind(imageOption, actors[position])
+            is EmptyViewHolder -> Toast.makeText(holder.itemView.context, "Nothing to bind in default holder", Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun getItemCount(): Int = actors.size
@@ -63,26 +74,28 @@ private class DataViewHolder(itemView: View) : ActorsViewHolder(itemView) {
     //  You have to update these view's content in runtime.
     //  First of all, find these views inside the "itemView" and store to variables.
     //  This "itemView" is a single recycler's item which is currently updating.
-    private val avatar: ImageView? = null
-    private val name: TextView? = null
-    private val oscarState: TextView? = null
+    private val avatar: ImageView? = itemView.findViewById(R.id.iv_actor_avatar)
+    private val name: TextView? = itemView.findViewById(R.id.tv_actor_name)
+    private val oscarState: TextView? = itemView.findViewById(R.id.tv_actor_oscar_state)
+
 
     // TODO 02: Rename this public method. Provide some data class into this method's parameters.
     //  This data class should contains actor's avatar url, name and if actor has oscar or not.
-    fun somePublicMethod(options: RequestOptions/*, data */) {
+    fun actorBind(options: RequestOptions, actor: Actor) {
         // TODO 03_01: Load an avatar picture.
         Glide.with(context)
-//            .load(actor.avatar)
-//            .apply(options)
-//            .into(avatar)
+            .load(actor.avatar)
+            .apply(options)
+            .into(avatar)
 
 
         // TODO 03_02: Setup new name.
+        name?.text = actor.name
 
         // TODO 03_03: Setup oscar state.
-        context.getString(
+        oscarState?.text = context.getString(
                 R.string.fragment_actors_avatar_oscar_state_text,
-                "replace with oscar state"
+                actor.hasOscar.toString()
         )
     }
 }
@@ -92,4 +105,5 @@ private val RecyclerView.ViewHolder.context
 
 // TODO 04: Create two Int constants for different ViewTypes.
 //  First represents the Empty View Holder. Second represents a ViewHolder with data.
-//private const val SAMPLE = -1
+private const val _EMPTY = 0
+private const val _ACTORS = 1
